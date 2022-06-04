0dee90e75c760a82ba157c716b439fe6ad77361d
Mauro Valdez <josenova0@gmail.com>
2022-06-03 21:22:43 -0300

    algunos cambios y avances

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 import './source/js/app.js';
-
-import './source/css/normalice.css';
+import './source/css/normalize.css';
 import './source/css/app.css';
--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,64 @@
 import ScrollReveal from "scrollreveal";
-
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-import 'boxicons';
+import 'boxicons'; 
+
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
+const navLink = document.querySelectorAll('.nav__link');
+const header = document.querySelector('#header');
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  menu();
+  homeSwiper();
+  scrollHeader();
+})
+
+function menu() {
+  if(navToggle) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu');
+    })
+  }
+  if(navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+    })
+  }
+  if(navLink) {
+    navLink.forEach(link => {
+      link.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+      })
+    })
+  }
+}
+
+function homeSwiper() {
+
+  const swiper = new Swiper('.home-swiper', {
+    spaceBetween:30,
+    loop: 'true',
+    speed: 1000,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+
+function scrollHeader() {
+  window.addEventListener('scroll', () => {
+    if(window.scrollY >= 50) {
+      header.classList.add('scroll-header');
+    } else {
+      header.classList.remove('scroll-header');
+    }
+  })
+}
